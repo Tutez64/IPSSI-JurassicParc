38ff2896d7e2a1a7008ae59711e59428eb5fba99
Bechir <kingbechir66@gmail.com>
2025-07-31 15:57:52 +0200

    feat: ajout de l'app.js avec routes, middlewares et port

---
+++ API_Parc1/config/sequelize.js
@@ -0,0 +1,21 @@
+// Importation des modules Sequelize
+const { Sequelize } = require('sequelize');
+
+// Connexion à PostgreSQL avec les variables d’environnement
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres', // on utilise PostgreSQL
+  logging: false,      // désactive les logs SQL dans la console
+});
+
+// Vérifie la connexion
+async function connectDB() {
+  try {
+    await sequelize.authenticate();
+    console.log(' Connexion PostgreSQL réussie');
+  } catch (error) {
+    console.error('Connexion PostgreSQL échouée :', error);
+  }
+}
+
+module.exports = { sequelize, connectDB };
